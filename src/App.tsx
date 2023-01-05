d9abdcb35ebfaa199e1e78f2ab3d960f51b2e0ad
Jalal999 <jmammadli2021@ada.edu.az>
2023-01-05 12:18:05 +0200

    Implemented Functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useMemo } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Container } from 'react-bootstrap';
+import NewNote from './components/NewNote';
+import { useLocalStorage } from './useLocalStorage';
+import { v4 as uuidV4 } from "uuid"
+import NoteList from './components/NoteList';
+import NoteLayout from './components/NoteLayout';
+import Note from './components/NoteLayout/Note';
+import EditNote from './components/EditNote';
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type NoteData = {
+  title: string,
+  markdown: string,
+  tags: Tag[]
+}
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type Tag = {
+  id: string,
+  label: string
+}
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  const onCreateNote = ({ tags, ...data }: NoteData) => {
+    setNotes(prevNotes => {
+      return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }]
+    })
+  }
+
+  const addTag = (tag: Tag) => {
+    setTags(prev => [...prev, tag])
+  }
+
+  const updateTag = (id: string, label: string) => {
+    setTags(prevTags => {
+      return prevTags.map(tag => {
+        if(tag.id === id) {
+          return { ...tag, label }
+        } else {
+          return tag
+        }
+      })
+    })
+  }
+
+  const deleteTag = (id: string) => {
+    setTags(prevTags => {
+      return prevTags.filter(tag => tag.id !== id)
+    })
+  }
+
+  function onUpdateNote(id: string, { tags, ...data }:NoteData) {
+    setNotes(prevNotes => {
+      return prevNotes.map(note => {
+        if(note.id === id) {
+          return {...note, ...data, tagIds: tags.map(tag => tag.id) }
+        } else {
+          return note;
+        }
+      })
+    })
+  }
+
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='my-4'>
+      <Routes>
+        <Route path="/" element={<NoteList notes={notesWithTags}  availableTags={tags} onUpdateTag={updateTag} onDeleteTag={deleteTag}/>} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
+          <Route index element={<Note onDelete={onDeleteNote} />} />
+          <Route path='edit' element={<EditNote  onSubmit={onUpdateNote} onAddTag={addTag} availableTags={tags} />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+
   );
 }
 
